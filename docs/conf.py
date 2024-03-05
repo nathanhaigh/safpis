0897409a4aeb63cad529f0b18dd83baa1ecdec66
Nathan Watson-Haigh <nathan.watson-haigh@grifols.com>
2024-03-05 14:39:09 +1030

    Move sys.path.insert() before import safpis

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,9 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath(".."))
 
 import safpis
-
-sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration ---------------------------------------------
 
